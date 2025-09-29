c26c4495f291feb73fb2f3cf4aa2d1e573dc6932
CauaOliveB <Cauaolivebarbosa@gmail.com>
2025-09-29 11:30:33 -0300

    Task Manager

---
+++ 3rdSemester/PWFE/SAEB_Challenge/Front/kanban/src/Routes/Routes.jsx
@@ -0,0 +1,9 @@
+import userCadaster from '../pages/userCadaster';
+import taskCadaster from '../pages/taskCadaster';
+import taskManager from '../pages/taskManager';
+import {Routes, Route} from ReactRouter-dom
+
+export function Routes() {
+
+
+}
